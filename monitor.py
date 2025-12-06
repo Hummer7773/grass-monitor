bcfa5ef9aa7675269f6fe36d03a56be98300205b
Hummer7773 <158740674+Hummer7773@users.noreply.github.com>
2025-12-06 09:02:20 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,9 +2,8 @@ import os
 import time
 import requests
 
-TOKEN = os.environ['8355023485:AAFLNjELSBozG5Nr73c7oyTclJIqqbohrkw']
-CHAT_ID = os.environ['6005418870']
-
+TOKEN = os.environ['BOT_TOKEN']
+CHAT_ID = os.environ['CHAT_ID']
 def send_alert(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     data = {'chat_id': CHAT_ID, 'text': msg}
